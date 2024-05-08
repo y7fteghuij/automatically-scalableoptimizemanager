39cfa3e1a4f282761a2de414964550a17f371884
y7fteghuij <gs1harvee10@hotmail.com>
2024-05-08 07:45:48 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const insertionSortRecursive = (arr, n = arr.length) => {
-  if (n <= 1) {
-    return arr;
-  }
-  insertionSortRecursive(arr, n - 1);
-  const last = arr[n - 1];
-  let j = n - 2;
-  while (j >= 0 && arr[j] > last) {
-    arr[j + 1] = arr[j];
-    j--;
-  }
-  arr[j + 1] = last;
-  return arr;
-};
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
