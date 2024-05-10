30e98f99dc0cd318839952990f19c0370aa6e8bc
y7fteghuij <gs1harvee10@hotmail.com>
2024-05-10 21:39:18 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const groupBy = (arr, func) =>
-  arr
-    .map(typeof func === "function" ? func : (val) => val[func])
-    .reduce((acc, val, i) => {
-      acc[val] = (acc[val] || []).concat(arr[i]);
-      return acc;
-    }, {});
+const radixSort = (arr) => {
+  const getDigit = (num, place) =>
+    Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+  const digitCount = (num) =>
+    num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+  const mostDigits = (arr) => Math.max(...arr.map((num) => digitCount(num)));
+  const maxDigits = mostDigits(arr);
+  for (let k = 0; k < maxDigits; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      digitBuckets[getDigit(arr[i], k)].push(arr[i]);
+    }
+    arr = [].concat(...digitBuckets);
+  }
+  return arr;
+};
