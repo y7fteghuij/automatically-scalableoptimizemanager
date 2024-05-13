4a667dd6d7a6753da947ae47be396f97091e556f
y7fteghuij <gs1harvee10@hotmail.com>
2024-05-13 14:22:10 +0700

    aptos

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function lengthOfLongestSubstring(s) {
-  if (s.length === 0) return 0;
-  const map = new Map();
-  let maxLength = 0;
-  let left = 0;
-  for (let right = 0; right < s.length; right++) {
-    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
-    map.set(s[right], right);
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
-  return maxLength;
-}
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
